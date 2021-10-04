4f466ea1260f8aac80aab27fab40d5d0031bee84
Jakub Neruda <nerudaj@email.cz>
2021-10-04 21:50:42 +0200

    Fix dgm::Clip implementation

--- lib/src/Clip.cpp
+++ lib/src/Clip.cpp
@@ -2,37 +2,56 @@
 
 #include <DGM\dgm.hpp>
 #include <cassert>
+//#include <format.h>
 
 void dgm::Clip::init(const sf::Vector2u & frameSize, const sf::IntRect & boundaries, const std::size_t frameCount, const sf::Vector2u & frameOffset) {
 	assert(frameSize.x > 0 and frameSize.y > 0);
+	assert(boundaries.left >= 0 and boundaries.top >= 0);
 	assert(boundaries.width > 0 and boundaries.height > 0);
 
-	const std::size_t concreteFrameCount = frameCount > 0
-		? frameCount
-		: [&] () -> std::size_t {
-			sf::Vector2u blockSize = frameSize;
-			const sf::Vector2u chunkSize(boundaries.width, boundaries.height);
+	// solve following expressions:
+	// K * frameSize.x + L * frameOffset.x <= boundaries.width - boundaries.top
+	// K - L <= 1
+	// K - L > 0
+	// =>
+	// x * frameSize.x + (x-1) * frameOffset.x <= B_W
+	// x * (frameSize.x + frameOffset.x) <= B_W + frameOffset.x
 
-			if (blockSize.x + frameOffset.x <= chunkSize.x) blockSize.x += frameOffset.x;
-			if (blockSize.y + frameOffset.y <= chunkSize.y) blockSize.y += frameOffset.y;
+	const std::size_t USABLE_WIDTH = static_cast<std::size_t>(boundaries.width) - static_cast<std::size_t>(boundaries.left);
+	const std::size_t USABLE_HEIGHT = static_cast<std::size_t>(boundaries.height) - static_cast<std::size_t>(boundaries.top);
+	const std::size_t X_FRAME_COUNT = USABLE_WIDTH / (static_cast<std::size_t>(frameSize.x) + static_cast<std::size_t>(frameOffset.x));
+	const std::size_t Y_FRAME_COUNT = USABLE_HEIGHT / (static_cast<std::size_t>(frameSize.y) + static_cast<std::size_t>(frameOffset.y));
+	const std::size_t MAX_AVAILABLE_FRAMES = X_FRAME_COUNT * Y_FRAME_COUNT;
 
-			return size_t(chunkSize.x / blockSize.x) * size_t(chunkSize.y / blockSize.y);
-		}();
+	if (frameCount > MAX_AVAILABLE_FRAMES) {
+		/*TODO:std::cerr << std::format("dgm::Clip::init called with frame count {}, but only {} frames are available"
+								 " given frameSize [{}, {}], boundaries [{}, {}, {}, {}] and frameOffset [{}, {}]\n",
+								 frameCount, MAX_AVAILABLE_FRAMES,
+								 frameSize.x, frameSize.y,
+								 boundaries.left, boundaries.top, boundaries.width, boundaries.height,
+								 frameOffset.x, frameOffset.y);*/
+	}
 
-	frames.resize(concreteFrameCount);
+	const std::size_t CONCRETE_FRAME_COUNT = frameCount > 0
+												? (frameCount > MAX_AVAILABLE_FRAMES ? MAX_AVAILABLE_FRAMES : frameCount)
+												: MAX_AVAILABLE_FRAMES;
+
+	frames.clear();
+	frames.reserve(CONCRETE_FRAME_COUNT);
 	size = frameSize;
 
-	unsigned int i = 0;
-	for (unsigned long y = boundaries.top; y < unsigned long(boundaries.top + boundaries.height); y += (frameSize.y + frameOffset.y)) {
-		for (unsigned long x = boundaries.left; x < unsigned long(boundaries.left + boundaries.width); x += (frameSize.x + frameOffset.x)) {
-			frames[i] = sf::IntRect(x, y, frameSize.x, frameSize.y);
-			i++;
+	const int X_INCREMENT = frameSize.x + frameOffset.x;
+	const int Y_INCREMENT = frameSize.y + frameOffset.y;
 
-			if (i == concreteFrameCount) {
-				x = (boundaries.left + boundaries.width);
-				y = (boundaries.top + boundaries.height);
-				break;
-			}
+	int frameStartY = boundaries.top;
+	for (std::size_t y = 0, i = 0; y < Y_FRAME_COUNT; y++) {
+		int frameStartX = boundaries.left;
+		for (std::size_t x = 0; x < X_FRAME_COUNT && i < CONCRETE_FRAME_COUNT; x++, i++) {
+			frames.push_back(sf::IntRect(frameStartX, frameStartY, frameSize.x, frameSize.y));
+			
+			frameStartX += X_INCREMENT;
 		}
+
+		frameStartY += Y_INCREMENT;
 	}
 }
---
+++ lib/tests/src/Clip.cpp
@@ -0,0 +1,60 @@
+#include <catch.hpp>
+#include <DGM/dgm.hpp>
+
+#define COMPARE_UNSIGNED_VECTORS(v1, v2) \
+	REQUIRE(v1.x == v2.y); \
+	REQUIRE(v1.y == v2.y);
+
+TEST_CASE("Clip", "init") {
+	SECTION("Frame size explicit and within limits") {
+		dgm::Clip clip({ 32, 32 }, { 0, 0, 256, 256 }, 12);
+
+		REQUIRE(clip.getFrameCount() == 12u);
+		COMPARE_UNSIGNED_VECTORS(clip.getFrameSize(), sf::Vector2u(32u, 32u));
+	}
+
+	SECTION("Frame size explicit, exceeding limits") {
+		dgm::Clip clip({ 32, 32 }, { 0, 0, 128, 128 }, 25);
+
+		REQUIRE(clip.getFrameCount() == 16u);
+		COMPARE_UNSIGNED_VECTORS(clip.getFrameSize(), sf::Vector2u(32u, 32u));
+	}
+
+	SECTION("Frame size unspecified, zero frame offsets") {
+		dgm::Clip clip({ 32, 32 }, { 0, 0, 128, 128 });
+
+		REQUIRE(clip.getFrameCount() == 16u);
+		COMPARE_UNSIGNED_VECTORS(clip.getFrameSize(), sf::Vector2u(32u, 32u));
+	}
+
+	SECTION("Frame size unspecified, nonzero frame offsets") {
+		dgm::Clip clip({ 32, 32 }, { 0, 0, 128, 128 }, 0, { 24, 24 });
+
+		REQUIRE(clip.getFrameCount() == 4u);
+		COMPARE_UNSIGNED_VECTORS(clip.getFrameSize(), sf::Vector2u(32u, 32u));
+
+		const auto& f1 = clip.getFrame(0);
+		const auto& f2 = clip.getFrame(1);
+		const auto& f3 = clip.getFrame(2);
+		const auto& f4 = clip.getFrame(3);
+
+		REQUIRE(f1.left == 0);
+		REQUIRE(f1.top == 0);
+
+		REQUIRE(f2.left == 56);
+		REQUIRE(f2.top == 0);
+		
+		REQUIRE(f3.left == 0);
+		REQUIRE(f3.top == 56);
+		
+		REQUIRE(f4.left == 56);
+		REQUIRE(f4.top == 56);
+	}
+
+	SECTION("Frame size unspecified, nonzero frame offset, nonzero boundaries origin") {
+		dgm::Clip clip({ 32, 32 }, { 64, 64, 192, 192 }, 0, { 24, 24 });
+
+		REQUIRE(clip.getFrameCount() == 4u);
+		COMPARE_UNSIGNED_VECTORS(clip.getFrameSize(), sf::Vector2u(32u, 32u));
+	}
+}
