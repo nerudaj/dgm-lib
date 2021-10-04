dec903b84daac4135fbfed0ec66eaba12d4635de
Jakub Neruda <nerudaj@email.cz>
2021-10-04 21:50:30 +0200

    Remove unused Conversion API

--- lib/include/DGM/classes/Conversion.hpp
+++ lib/include/DGM/classes/Conversion.hpp
@@ -1,48 +1,10 @@
 #pragma once
 
 #include <DGM\dgm.hpp>
-#include <optional>
 
 namespace dgm {
 	class Conversion {
 	public:
-		/**
-		 * \brief Takes a string with hex representation of a color (#XXXXXX or #XXX)
-		 * and converts it to output sf::Color
-		 *
-		 * If color is not valid, black is returned
-		 */
-		static [[nodiscard]] sf::Color stringToColor(std::string str);
-
-		/**
-		 * \brief Takes a string of integer numbers separated with delimiter
-		 * splits it to integer array and returns pointer to newly allocated
-		 * array. Size of array is stored in size.
-		 *
-		 * \returns NULL on failure, valid pointer to int array otherwise
-		 */
-		static [[nodiscard]] std::optional<std::vector<int>> stringToIntArray(const char delimiter, const std::string &str);
-
-		/**
-		 * \brief Takes a string of integer numbers separated with delimiter
-		 * and splits it to sf::Vector2i
-		 *
-		 * \details On failure, dst is not modified
-		 *
-		 * \returns TRUE if str contains only precisely two int numbers. FALSE otherwise
-		 */
-		static [[nodiscard]] std::optional<sf::Vector2i> stringToVector2i(const char delimiter, const std::string &str);
-
-		/**
-		* \brief Takes a string of integer numbers separated with delimiter
-		* and splits it to sf::IntRect
-		*
-		* \details On failure, dst is not modified
-		*
-		* \returns TRUE if str contains precisely four int numbers. FALSE otherwise
-		*/
-		static [[nodiscard]] std::optional<sf::IntRect> stringToIntRect(const char delimiter, const std::string &str);
-
 		/**
 		 *  \brief Convert polar coordinates to cartesian
 		 *
--- lib/src/Conversion.cpp
+++ lib/src/Conversion.cpp
@@ -1,83 +1,9 @@
 #include <DGM\dgm.hpp>
-#include <regex>
-#include <algorithm>
-#include <Strings.hpp>
 
 const float PIOVER180 = 0.01745329252f;
 const float _180OVERPI = 57.2957795131f;
 
-using std::vector;
-using std::string;
 using dgm::Conversion;
-
-uint8_t nibbleToUint8(const char bit) {
-	if ('a' <= bit && bit <= 'f') return uint8_t(bit - 'a' + 10);
-	return uint8_t(bit - '0');
-}
-
-uint8_t hexToUint8(const std::string &hex) {
-	uint8_t result = 0;
-	for (auto bit : hex) {
-		result *= 16;
-		result += nibbleToUint8(bit);
-	}
-	return result;
-}
-
-inline void uppercaseToLowercase(std::string &str) {
-	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-}
-
-sf::Color Conversion::stringToColor(std::string str) {
-	const std::regex hexaShort("#[0-9a-f]{3}");
-	const std::regex hexaLong("#[0-9a-f]{6}");
-
-	uppercaseToLowercase(str);
-	
-	uint8_t colorBits[3] = { 0 };
-	
-	if (std::regex_match(str, hexaShort)) {
-		for (unsigned i = 1; i < str.size(); i++) {
-			colorBits[i] = hexToUint8(str.substr(i, 1));
-		}
-	}
-	else if (std::regex_match(str, hexaLong)) {
-		for (unsigned i = 1, p = 0; i < str.size(); i += 2, p++) {
-			colorBits[p] = hexToUint8(str.substr(i, 2));
-		}
-	}
-
-	return sf::Color(colorBits[0], colorBits[1], colorBits[2]);
-}
-
-std::optional<vector<int>> Conversion::stringToIntArray(const char delimiter, const std::string & str) {
-	vector<string> split;
-	Strings::split(delimiter, str, split);
-	
-	vector<int> result(split.size());
-
-	for (size_t i = 0; i < split.size(); i++) {
-		try {
-			result[i] = std::stoi(split[i]);
-		} catch (...) {
-			return std::nullopt;
-		}
-	}
-
-	return result;
-}
-
-std::optional<sf::Vector2i> Conversion::stringToVector2i(const char delimiter, const std::string& str) {
-	auto arr = Conversion::stringToIntArray(delimiter, str);
-	if (!arr.has_value() || arr.value().size() != 2) return std::nullopt;
-	return sf::Vector2i(arr.value()[0], arr.value()[1]);
-}
-
-std::optional<sf::IntRect> Conversion::stringToIntRect(const char delimiter, const std::string& str) {
-	auto arr = Conversion::stringToIntArray(delimiter, str);
-	if (!arr.has_value() || arr.value().size() != 4) return std::nullopt;
-	return sf::IntRect(arr.value()[0], arr.value()[1], arr.value()[2], arr.value()[3]);
-}
 
 sf::Vector2f Conversion::cartesianToPolar(const float x, const float y) {
 	float size = dgm::Math::vectorSize(x, y);
