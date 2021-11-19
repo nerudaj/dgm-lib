1b0b0e56c5bc351f4f3190f28802f83e699640a4
Jakub Neruda <nerudaj@email.cz>
2021-11-19 14:15:40 +0100

    Add rotation methods for dgm::ps::Particle

--- lib/src/Particle.cpp
+++ lib/src/Particle.cpp
@@ -9,15 +9,39 @@ void Particle::setAnimationFrame(const sf::IntRect &frame) noexcept {
 	quad[3].texCoords = sf::Vector2f(float(frame.left), float(frame.top + frame.height));
 }
 
+// This just makes a unit vector that has angle of 45°
+constexpr float SIN_OF_45_DEG = 0.707106f;
+const sf::Vector2f VEC_45_DEG = sf::Vector2f(SIN_OF_45_DEG, -SIN_OF_45_DEG);
+
+void dgm::ps::Particle::setRotation(const float angle) noexcept {
+	rotation = angle;
+
+	// Backup position of the center of the particle
+	// and create a unit vector that has been rotated so it points to position of first vertex after rotation
+	// and is scaled to point to one of the new vertex positions
+	const auto POS = getPosition();
+	const auto BASE_VEC = dgm::Math::rotateVector(VEC_45_DEG, rotation) * diagonalHalfLength;
+	
+	// So the first vertex is just offset from center POS in direction of BASE_VEC
+	// Each subsequent vertex is perpendicular to the previous one so we do the same and just alter
+	// signs and order of X and Y components
+	quad[0].position = POS + sf::Vector2f(BASE_VEC.y, -BASE_VEC.x);
+	quad[1].position = POS + BASE_VEC;
+	quad[2].position = POS + sf::Vector2f(-BASE_VEC.y, BASE_VEC.x);
+	quad[3].position = POS - BASE_VEC;
+}
+
 void Particle::spawn(const sf::Vector2f& newPosition, const sf::Vector2f& newSize, const sf::Time& newLifespan) {
 	lifespan = newLifespan.asSeconds();
-	size = newSize;
+	const auto halfSize = newSize / 2.f;
+	rotation = 0.f;
+	diagonalHalfLength = dgm::Math::vectorSize(halfSize);
 
 	sf::Vector2f offsets[] = {
-		{0.f, 0.f},
-		{size.x, 0.f},
-		{size.x, size.y},
-		{0.f, size.y}
+		{-halfSize.x, -halfSize.y},
+		{halfSize.x, -halfSize.y},
+		{halfSize.x, halfSize.y},
+		{-halfSize.x, halfSize.y}
 	};
 	
 	for (int i = 0; i < 4; i++) {
---
+++ sandbox/Main.cpp
@@ -0,0 +1,47 @@
+﻿#include <DGM/dgm.hpp>
+
+class RotatingParticleSystem : public dgm::ps::ParticleSystemInterface {
+	float rotationSpeed = 75.f; // degrees per second
+
+public:
+	virtual void update(const dgm::Time& time) override {
+		particles[0]->rotateBy(rotationSpeed * time.getDeltaTime());
+	}
+
+	virtual void init(const std::size_t particleCount) override {
+		ParticleSystemInterface::init(particleCount);
+
+		particles.expand();
+		auto& p = particles.last();
+		p->spawn(sf::Vector2f(1920.f, 1080.f) / 2.f, sf::Vector2f(256.f, 256.f), sf::Time::Zero);
+		p->setColor(sf::Color::Red);
+	}
+};
+
+int main(int argc, char *argv[]) {
+	dgm::Window window({ 1920, 1080 }, "Sandbox", false);
+	sf::Event event;
+	dgm::Time time;
+
+	RotatingParticleSystem rps;
+	rps.init(1);
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) window.close();
+		}
+
+		// Logic
+		time.reset();
+		rps.update(time);
+
+		// Draw
+		window.beginDraw();
+
+		rps.draw(window);
+
+		window.endDraw();
+	}
+
+	return 0;
+}
