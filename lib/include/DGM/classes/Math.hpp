0474b96a11ba67b198c354e7b35bd361c109a22e
Jakub Neruda <nerudaj@email.cz>
2022-05-28 18:25:29 +0200

    Added Math::radialDifference for computing difference between angles

--- lib/include/DGM/classes/Math.hpp
+++ lib/include/DGM/classes/Math.hpp
@@ -4,11 +4,13 @@
 #include <cmath>
 #include <cassert>
 
-namespace dgm {
+namespace dgm
+{
 	/**
 	 *  \brief Class with auxiliary mathematical methods
 	 */
-	class Math {
+	class Math
+	{
 	public:
 		constexpr static float PI = 3.1415926536f;
 		constexpr static float PIOVER180 = PI / 180.f;  /// multiply this with degrees to get radians
@@ -16,63 +18,82 @@ namespace dgm {
 
 		/**
 		 *  \brief Compute size of the vector
-		 *  
+		 *
 		 *  \param[in]  vect  Vector object
-		 *  
+		 *
 		 *  \return Size of vect
 		 */
-		static float vectorSize(const sf::Vector2f &vect) {
+		static float vectorSize(const sf::Vector2f& vect)
+		{
 			return sqrt(vect.x * vect.x + vect.y * vect.y);
 		}
 
-		static float vectorSize(const float x, const float y) {
+		static float vectorSize(const float x, const float y)
+		{
 			return vectorSize({ x, y });
 		}
 
-		static sf::Vector2f rotateVector(const sf::Vector2f& vec, float angle) {
+		static sf::Vector2f rotateVector(const sf::Vector2f& vec, float angle)
+		{
 			const float rad = angle * PIOVER180;
 			return sf::Vector2f(
 				vec.x * std::cos(rad) - vec.y * std::sin(rad),
 				vec.x * std::sin(rad) + vec.y * std::cos(rad)
 			);
 		}
-		
+
 		/**
 		 *  \brief Computes dot product of two vectors
-		 *  
+		 *
 		 *  \param[in]  vectorA  Vector object
 		 *  \param[in]  vectorB  Vector object
 		 *
 		 *  \return Dot product of vectorA and vectorB
 		 */
-		static float dotProduct(const sf::Vector2f &vectorA, const sf::Vector2f &vectorB) {
+		static float dotProduct(const sf::Vector2f& vectorA, const sf::Vector2f& vectorB)
+		{
 			return vectorA.x * vectorB.y + vectorA.x * vectorB.y;
 		}
-		
+
 		/**
 		 *  \brief Map the value from interval X to interval Y (linearly)
-		 *  
+		 *
 		 *  \param[in]  value   Value to map
 		 *  \param[in]  startX  Start of X interval
 		 *  \param[in]  endX    End of X interval
 		 *  \param[in]  startY  Start of Y interval
 		 *  \param[in]  endY    End of Y interval
 		 */
-		static float map(const float value, const float startX, const float endX, const float startY, const float endY) {
+		static float map(const float value, const float startX, const float endX, const float startY, const float endY)
+		{
 			return  (endY - startY) / (endX - startX) * (value - startX) + startY;
 		}
 
 		/**
 		 * \brief Test whether a float value is approximately equal to another float value
-		 * 
+		 *
 		 * \param[in]  value          Value to test
 		 * \param[in]  expectedValue  Expected value
 		 * \param[in]  precision      Precision of comparison
 		 *
 		 * \return TRUE if values are equal within a given precision
 		 */
-		static bool approxEqual(const float value, const float expectedValue, const float precision = 0.01f) {
+		static bool approxEqual(const float value, const float expectedValue, const float precision = 0.01f)
+		{
 			return std::abs(expectedValue - value) <= precision;
+		}
+
+		/**
+		 * \brief Compute difference between two angles, wrapping over 360°
+		 *
+		 * \param[in] alfa  Angle in degrees in interval <0, 360>
+		 * \param[in] beta  Angle in degrees in interval <0, 360>
+		 */
+		static float radialDifference(const float alfa, const float beta)
+		{
+			return std::min(
+				std::abs(alfa - beta),
+				std::min(alfa, beta) + 360 - std::max(alfa, beta));
 		}
 	};
 };
--- lib/tests/src/Math.cpp
+++ lib/tests/src/Math.cpp
@@ -8,15 +8,25 @@
 	REQUIRE(NUMBER_DISTANCE(v1.x, v2.x) < 0.0001f); \
 	REQUIRE(NUMBER_DISTANCE(v1.y, v2.y) < 0.0001f);
 
-TEST_CASE("RotateVector", "Math") {
+TEST_CASE("RotateVector", "Math")
+{
 	auto v = dgm::Math::rotateVector({ 1.f, 0.f }, 90.f);
 	std::cout << v.x << ", " << v.y << std::endl;
 
 	COMPARE_VECTORS(v, sf::Vector2f(0.f, 1.f));
 }
 
-TEST_CASE("VectorSize", "Math") {
+TEST_CASE("VectorSize", "Math")
+{
 	REQUIRE(dgm::Math::vectorSize(0.f, 0.f) == 0.f);
 	REQUIRE(dgm::Math::vectorSize(3.f, 4.f) == 5.f);
 	REQUIRE(dgm::Math::vectorSize(-3.f, 4.f) == 5.f);
+}
+
+TEST_CASE("Radial difference", "Math")
+{
+	REQUIRE(dgm::Math::radialDifference(0.f, 10.f) == 10.f);
+	REQUIRE(dgm::Math::radialDifference(350.f, 0.f) == 10.f);
+	REQUIRE(dgm::Math::radialDifference(270.f, 90.f) == 180.f);
+	REQUIRE(dgm::Math::radialDifference(360.f, 0.f) == 0.f);
 }
