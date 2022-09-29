e94ae67ef31dad3813857f0c97aedfb246d0da53
Jakub Neruda <nerudaj@email.cz>
2022-09-29 22:28:36 +0200

    Fix missing virtual dtors

--- lib/include/DGM/classes/Controller.hpp
+++ lib/include/DGM/classes/Controller.hpp
@@ -2,7 +2,7 @@
  *  \file Controller.hpp
  *  \author doomista
  */
- 
+
 #pragma once
 
 #include <map>
@@ -12,24 +12,26 @@
 #include <Windows.h>
 #include <Xinput.h>
 
-namespace dgm {
+namespace dgm
+{
 	/**
 	 *  \brief Abstraction over physical input devices
-	 * 
+	 *
 	 *  Inputs are abstracted through action codes. Concrete implementation
 	 *  should provide a way to bind physical inputs of chosen devices to
 	 *  abstract action codes.
 	 */
-	class AbstractController {
+	class AbstractController
+	{
 	public:
 		/**
 		 *  \brief Returns true if input associated with code is toggled
 		 */
 		[[nodiscard]] virtual bool isToggled(const int code) const = 0;
-		
+
 		/**
 		 *  \brief Marks input as released
-		 * 
+		 *
 		 *  Function isToggled will not return TRUE until underlying physical
 		 *  input is released.
 		 */
@@ -44,11 +46,13 @@ namespace dgm {
 	/**
 	 *  \brief Xbox controller related data structures
 	 */
-	namespace Xbox {
+	namespace Xbox
+	{
 		/**
 		 *  \brief Enumerator for buttons on Xbox controller
 		 */
-		enum class Button : std::size_t {
+		enum class Button : std::size_t
+		{
 			Unknown = 0x0000,
 			DPadUp = 0x0001,
 			DPadDown = 0x0002,
@@ -69,7 +73,8 @@ namespace dgm {
 		/**
 		 *  \brief Enumeration of non-binary axii on Xbox controller
 		 */
-		enum class Axis : std::size_t {
+		enum class Axis : std::size_t
+		{
 			Unknown = 0,
 			LTrigger = 1,
 			RTrigger,
@@ -88,20 +93,22 @@ namespace dgm {
 	 *  \brief Concrete implementation of controller that interfaces
 	 *  with keyboard, mouse and Xbox controller or controllers that
 	 *  use the XInput drivers.
-	 * 
+	 *
 	 *  Expected workflow is to call bindInput to bind keys and buttons
 	 *  to particular numerical code representing certain action.
-	 * 
+	 *
 	 *  You can then call update, keyPressed and getAxísValue with
 	 *  those codes to get current state for that input, disregarding
 	 *  of physical input device.
-	 * 
+	 *
 	 *  You can even bind for example keyboard key and xbox controller
 	 *  button to the same input code.
 	 */
-	class Controller : public AbstractController {
+	class Controller : public AbstractController
+	{
 	protected:
-		struct Binding {
+		struct Binding
+		{
 			bool released = false;
 			float negateMultiplier = 1.f;
 			sf::Keyboard::Key key = sf::Keyboard::Key::Unknown;
@@ -118,7 +125,7 @@ namespace dgm {
 	public:
 		/**
 		 *  \brief Update state on xbox controller
-		 * 
+		 *
 		 *  You only need to call this method if you intend on using
 		 *  the controller. You don't need to call this if you only
 		 *  use mouse and keyboard.
@@ -127,21 +134,22 @@ namespace dgm {
 
 		/**
 		 *  \brief Test if controller is connected
-		 * 
+		 *
 		 *  \pre setControllerIndex was called, otherwise index 0 is used
 		 *  \pre update was called, this method returns value valid since last update call
 		 */
-		[[nodiscard]] bool isControllerConnected() const noexcept {
+		[[nodiscard]] bool isControllerConnected() const noexcept
+		{
 			return controllerConnected;
 		}
 
 		/**
 		 * \brief Test whether particular input code is pressed
-		 * 
+		 *
 		 * If input code was previously released using releaseKey then
 		 * physical key/button must first be released, this function called
 		 * and only then will this function return TRUE again.
-		 * 
+		 *
 		 * This function does NOT work for dgm::Xbox::Axis bindings (use getAxisValue
 		 * for those).
 		 */
@@ -159,28 +167,29 @@ namespace dgm {
 		 * \note In order to function properly, keyPressed should be called
 		 * every frame (to ensure that controller will notice the released key)
 		 */
-		void releaseKey(const int code) noexcept {
+		void releaseKey(const int code) noexcept
+		{
 			bindings[code].released = true;
 		}
 
 		/**
 		 *  \brief Get analog value of bound action code
-		 * 
+		 *
 		 *  If stick X/Y axis is bound to action, value from -1.f to 1.f will be returned
 		 *  based on the position of the stick.
-		 * 
+		 *
 		 *  Both triggers return value from 0.f to 1.f. If button or keyboard key is
 		 *  bound to this action code as well, this function will return 1.f if such
 		 *  input is pressed.
-		 * 
+		 *
 		 *  If stick X/Y pos axis is bound to action, value from 0.f to 1.f will be
 		 *  returned based on the position of the stick only on positive half of range.
 		 *  If button or keyboard key is bound to this action code as well, this function
 		 *  will return 1.f if such input is pressed.
-		 * 
+		 *
 		 *  Similar applies to negative half of the stick range - function returns value
 		 *  from -1.f to 0.f and -1.f for buttons.
-		 * 
+		 *
 		 *  \warn While you technically can bind a button to action code that is also
 		 *  bound to general X/Y stick axis (not positive or negative halves) it doesn't
 		 *  make sense from usage perspective. Stick can return range <-1.f, 1.f> but button
@@ -211,7 +220,8 @@ namespace dgm {
 		/**
 		 *  \brief Set index of required controller
 		 */
-		void setControllerIndex(const unsigned short index) noexcept {
+		void setControllerIndex(const unsigned short index) noexcept
+		{
 			controllerIndex = index;
 		}
 
@@ -221,6 +231,6 @@ namespace dgm {
 		void setControllerVibration(const float leftMotorForce, const float rightMotorForce);
 
 		Controller() {}
-		~Controller() {}
+		virtual ~Controller() = default;
 	};
 }
