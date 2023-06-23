08690a57485f32b49e4094b88e6630f9bc83c249
Jakub Neruda <nerudaj@email.cz>
2023-06-24 00:39:41 +0200

    Error handling has been revamped

--- lib/include/DGM/classes/Error.hpp
+++ lib/include/DGM/classes/Error.hpp
@@ -1,42 +1,25 @@
 #pragma once
 
 #include <expected>
+#include <format>
+#include <stacktrace>
 #include <stdexcept>
 #include <string>
 
 namespace dgm
 {
-    class GeneralException : public std::runtime_error
+    class Exception : public std::runtime_error
     {
     public:
-        GeneralException(const std::string& message)
-            : std::runtime_error(message)
+        Exception(
+            const std::string& message,
+            std::stacktrace trace = std::stacktrace::current())
+            : std::runtime_error(std::format(
+                "Error message: {}\n\nStacktrace: \n{}",
+                message,
+                std::to_string(trace)))
         {
         }
-    };
-
-    class ResourceException : public std::runtime_error
-    {
-    public:
-        ResourceException(const std::string& message)
-            : std::runtime_error(message)
-        {
-        }
-    };
-
-    class EnvironmentException : public std::runtime_error
-    {
-    public:
-        EnvironmentException(const std::string& message)
-            : std::runtime_error(message)
-        {
-        }
-    };
-
-    class ShellException : public std::runtime_error
-    {
-    public:
-        ShellException(const std::string& msg) : std::runtime_error(msg) {}
     };
 
     using ErrorMessage = std::string;
