62da54eeebd832bf7c6b3ebfd7e9d850fda1ee48
Jakub Neruda <nerudaj@email.cz>
2024-09-29 18:30:38 +0200

    Example 01: TileMap - Final

---
+++ examples/example-01-tilemap/Main.cpp
@@ -0,0 +1,78 @@
+/*
+An example showing a built-in class for rendering 2D tilemaps.
+Implementation of dgm::TileMap is based on SFML tutorials:
+https://www.sfml-dev.org/tutorials/2.6/graphics-vertex-array.php
+
+TileMap is more efficient to render than to render tiles individually.
+*/
+
+#include "../shared/DemoData.hpp"
+#include <DGM/dgm.hpp>
+
+dgm::TileMap createSampleTilemap(const dgm::ResourceManager& resmgr)
+{
+    auto tilemap = dgm::TileMap(
+        resmgr.get<sf::Texture>("tileset.png").value().get(),
+        resmgr.get<dgm::Clip>("tileset.png.clip").value().get());
+
+    // clang-format off
+    tilemap.build(
+        {64u, 64u}, // size of each tile in pixels
+        { // vector of indices into dgm::Clip given to the dgm::TileMap
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+            1, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 2, 2, 1, 
+            1, 0, 3, 3, 0, 0, 0, 3, 0, 0, 0, 3, 3, 2, 1, 
+            1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 2, 1, 
+            1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 2, 1, 
+            1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 2, 1, 
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1, 
+            1, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 1, 
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        },
+        {15, 10}); // 15x10 tiles
+    // clang-format on
+
+    // NOTE: Once constructed, a texture/clip is bound to a tilemap
+    // but tilemap can be rebuilt many times.
+    // You can even call changeTile to alter individual tiles (slow).
+
+    return tilemap;
+}
+
+int main()
+{
+    dgm::Window window({ 1280, 720 }, "Example: Tileset", false);
+
+    dgm::ResourceManager resmgr;
+    // Normally, you want to intercept return value from
+    // loadResourcesFromDirectory but this is a sample, so we're ignoring the
+    // result.
+    std::ignore = resmgr.loadResourcesFromDirectory<dgm::Clip>(
+        RESOURCE_DIR, dgm::Utility::loadClip, { ".clip" });
+    std::ignore = resmgr.loadResourcesFromDirectory<sf::Texture>(
+        RESOURCE_DIR, dgm::Utility::loadTexture, { ".png" });
+
+    auto tilemap = createSampleTilemap(resmgr);
+
+    sf::Event event;
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                std::ignore = window.close();
+            }
+        }
+
+        /* DRAW */
+        window.beginDraw();
+
+        window.draw(tilemap); // draws like a native sf primitive
+
+        window.endDraw();
+    }
+
+    return 0;
+}
