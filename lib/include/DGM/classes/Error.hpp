896c6ea6cbd7690786acacb6f57d8c452ce0cd18
Jakub Neruda <nerudaj@email.cz>
2025-01-07 20:30:09 +0100

    Introduce `dgm::Error`

--- lib/include/DGM/classes/Error.hpp
+++ lib/include/DGM/classes/Error.hpp
@@ -15,14 +15,37 @@ namespace dgm
             const std::string& message,
             std::stacktrace trace = std::stacktrace::current())
             : std::runtime_error(std::format(
-                "Error message: {}\n\nStacktrace: \n{}",
-                message,
-                std::to_string(trace)))
+                  "Error message: {}\n\nStacktrace: \n{}",
+                  message,
+                  std::to_string(trace)))
         {
         }
     };
 
     using ErrorMessage = std::string;
+    class [[nodiscard]] Error final
+    {
+    public:
+        Error(const std::string& message)
+            : message(message), trace(std::stacktrace::current())
+        {
+        }
+
+    public:
+        const std::string& getMessage() const noexcept
+        {
+            return message;
+        }
+
+        const std::stacktrace& getTrace() const noexcept
+        {
+            return trace;
+        }
+
+    private:
+        std::string message;
+        std::stacktrace trace;
+    };
 
     using ExpectedSuccess = std::expected<std::true_type, ErrorMessage>;
 } // namespace dgm
