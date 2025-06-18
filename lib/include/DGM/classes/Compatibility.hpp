0104fa6bb06b4f085abb2796b628e077d8316d3e
Jakub Neruda <nerudaj@email.cz>
2025-06-18 08:59:23 +0200

    Update Compatibility.hpp

--- lib/include/DGM/classes/Compatibility.hpp
+++ lib/include/DGM/classes/Compatibility.hpp
@@ -10,6 +10,24 @@
 #define CONSTEXPR_NODISCARD constexpr
 #endif
 
+#include <ranges>
+#include <vector>
+
+namespace std
+{
+    namespace ranges
+    {
+        namespace views
+        {
+            auto enumerate(auto&& r)
+            {
+                return std::views::zip(
+                    std::views::iota(0, static_cast<ptrdiff_t>(r.size())), r);
+            }
+        } // namespace views
+    } // namespace ranges
+} // namespace std
+
 #else
 #ifndef NODISCARD_RESULT
 #define NODISCARD_RESULT [[nodiscard]]
