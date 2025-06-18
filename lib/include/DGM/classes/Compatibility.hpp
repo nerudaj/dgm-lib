cd196ea4e9b6da8daf7efe153b8a4849e7caa387
Jakub Neruda <nerudaj@email.cz>
2025-06-18 08:47:34 +0200

    Remove unused compat methods

--- lib/include/DGM/classes/Compatibility.hpp
+++ lib/include/DGM/classes/Compatibility.hpp
@@ -10,29 +10,6 @@
 #define CONSTEXPR_NODISCARD constexpr
 #endif
 
-#include <ranges>
-#include <vector>
-
-namespace std
-{
-    namespace ranges
-    {
-        template<class Container, class... Args>
-        Container to(Args&&... args)
-        {
-            return Container(std::forward<Args>(args)...);
-        }
-
-        namespace views
-        {
-            auto enumerate(auto&& r)
-            {
-                return std::views::zip(
-                    std::views::iota(0, static_cast<ptrdiff_t>(r.size())), r);
-            }
-        } // namespace views
-    } // namespace ranges
-} // namespace std
 #else
 #ifndef NODISCARD_RESULT
 #define NODISCARD_RESULT [[nodiscard]]
